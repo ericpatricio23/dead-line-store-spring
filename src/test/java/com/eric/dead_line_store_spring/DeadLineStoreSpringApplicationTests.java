afd8f3ff0b6e1d78858a62c1b818f8c4a74e4cb9
eric patricio <erecpatricio@gmail.com>
2026-05-06 23:32:51 -0300

    fix: ativa profile de teste no junit

--- src/test/java/com/eric/dead_line_store_spring/DeadLineStoreSpringApplicationTests.java
+++ src/test/java/com/eric/dead_line_store_spring/DeadLineStoreSpringApplicationTests.java
@@ -2,12 +2,13 @@ package com.eric.dead_line_store_spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DeadLineStoreSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
